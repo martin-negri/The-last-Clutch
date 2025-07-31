ed6e585ba6061c09ec3112eef87773f8a7d7ff1e
Tincho <martin.negri@outlook.es>
2025-07-31 15:09:12 -0300

    Seccion inicio completa

--- js/main.js
+++ js/main.js
@@ -4,8 +4,12 @@ const cerrar = document.querySelector("#cerrar");
 
 abrir.addEventListener("click", () => {
     nav.classList.add("visible");
-})
+    abrir.classList.add("oculto");
+    cerrar.classList.remove("oculto");
+});
 
 cerrar.addEventListener("click", () => {
     nav.classList.remove("visible");
-})
+    cerrar.classList.add("oculto");
+    abrir.classList.remove("oculto");
+});
